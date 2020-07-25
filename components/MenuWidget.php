ba4e224f0a2c7886a0b2246f7475e4411485a7ae
ruslan <musaviruslan.1996@gmail.com>
2020-07-25 23:49:37 +0300

    all

--- components/MenuWidget.php
+++ components/MenuWidget.php
@@ -21,14 +21,41 @@ public  function  init(){
     public  function  run(){
 //        return 'MENU';
         //return $this->tpl;
-       $this->data = Category::find()->indexBy('id  ')->all();
-
-       debug($this->data);
-       return 1;
+       $this->data = Category::find()->indexBy('id')->asArray()->all();
+       $this->tree = $this->getTree();
+       $this->menuHtml  = $this->getMenuHtml($this->tree);
+        //debug($this->tree);
+        return $this->menuHtml;
 
     }
     protected  function  getTree(){
+    $tree = [];
+    foreach ( $this->data as $id=>&$node){
+        if (!$node['parent_id']){
+            $tree[$id] = &$node;
+        }
+            else{
+                $this->data[$node['parent_id']]['childs'][$node['id']] = &$node;
+            }
+        }
 
-    }
+        return $tree;
+        }
+
+        protected  function getMenuHtml($tree){
+            $str = '';
+            foreach ($tree as $category){
+
+                $str .= $this->catToTemplate($category);
+            }
+             return $str;
+        }
+
+        protected function  catToTemplate($category){
+            ob_start();
+            include __DIR__ . '/menu_tpl/'. $this->tpl;
+            return ob_get_clean();
+
+        }
 
 }
--- models/Product.php
+++ models/Product.php
@@ -14,6 +14,6 @@ class Product extends  ActiveRecord
     }
 
     public  function  getCategory(){
-        return $this->hasMany(Category::className, ['id' => 'category_id']);
+        return $this->hasOne(Category::className, ['id' => 'category_id']);
 }
 }
